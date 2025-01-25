709c29290223b1ef505898f18ce58b6a91f24d28
warrior <1986411713@qq.com>
2025-01-25 23:33:51 +0800

    添加Swagger相关配置来调试项目

--- src/main/java/com/hmdp/config/WebMVCConfig.java
+++ src/main/java/com/hmdp/config/WebMVCConfig.java
@@ -2,11 +2,21 @@ package com.hmdp.config;
 
 import com.hmdp.utils.LoginCheckInterceptor;
 import com.hmdp.utils.LoginInterceptor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
 
+@Slf4j
 @Configuration
 public class WebMVCConfig implements WebMvcConfigurer {
 
@@ -31,5 +41,36 @@ public class WebMVCConfig implements WebMvcConfigurer {
                         "/user/login"
                 )
                 .order(1);
+    }
+
+    /**
+     * 配置Kni4fe调试文档（该方法可配置多个用于分页）
+     * @return
+     */
+    @Bean
+    public Docket docket() {
+        log.info("准备生成接口文档");
+        ApiInfo apiInfo = new ApiInfoBuilder()
+                .title("黑马点评项目接口文档")
+                .version("2.0")
+                .description("黑马点评项目接口文档")
+                .build();
+        Docket docket = new Docket(DocumentationType.SWAGGER_2)
+                .apiInfo(apiInfo)
+                .select()
+                .apis(RequestHandlerSelectors.basePackage("com.hmdp.controller"))
+                .paths(PathSelectors.any())
+                .build();
+        return docket;
+    }
+
+    /**
+     * 设置静态资源映射
+     * @param registry
+     */
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        log.info("准备设置静态资源映射");
+        registry.addResourceHandler("/doc.html").addResourceLocations("classpath:/META-INF/resources/");
+        registry.addResourceHandler("/webjars/**").addResourceLocations("classpath:/META-INF/resources/webjars/");
     }
 }
