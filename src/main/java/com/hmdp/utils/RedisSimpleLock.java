bf8835a2a2bf09ee963fb239db23dab6084c53a4
warrior <1986411713@qq.com>
2025-02-10 15:02:44 +0800

    使用Redis分布式锁实现一人一单功能，解决集群状态下的线程安全问题（完善分布式锁，解决误删问题）

--- src/main/java/com/hmdp/utils/RedisSimpleLock.java
+++ src/main/java/com/hmdp/utils/RedisSimpleLock.java
@@ -1,5 +1,6 @@
 package com.hmdp.utils;
 
+import cn.hutool.core.lang.UUID;
 import cn.hutool.core.util.BooleanUtil;
 import org.springframework.data.redis.core.StringRedisTemplate;
 
@@ -8,6 +9,8 @@ import java.util.concurrent.TimeUnit;
 public class RedisSimpleLock implements ILock{
     private StringRedisTemplate stringRedisTemplate;
     private String name;
+
+    private String id_prefix = UUID.randomUUID().toString(true) + "-";
 
     private static final String lock_prefix = "lock:";
 
@@ -20,12 +23,19 @@ public class RedisSimpleLock implements ILock{
     public boolean tryLock(long timeoutSec) {
         String key = lock_prefix + name;
         long id = Thread.currentThread().getId();
-        Boolean aBoolean = stringRedisTemplate.opsForValue().setIfAbsent(key, id + "", timeoutSec, TimeUnit.SECONDS);
+        //使用UUID加线程ID来标识锁
+        String val = id_prefix + id;
+        Boolean aBoolean = stringRedisTemplate.opsForValue().setIfAbsent(key, val, timeoutSec, TimeUnit.SECONDS);
         return BooleanUtil.isTrue(aBoolean);
     }
 
     @Override
     public void unlock() {
-        stringRedisTemplate.delete(lock_prefix + name);
+        String s = stringRedisTemplate.opsForValue().get(lock_prefix + name);
+        long id = Thread.currentThread().getId();
+        String ThreadId = id_prefix + id;
+        //如果锁识别成功，那么释放锁
+        if (ThreadId.equals(s))
+            stringRedisTemplate.delete(lock_prefix + name);
     }
 }
