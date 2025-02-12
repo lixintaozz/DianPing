9a852ed25c61427c67e7b6c41bcf6bfb6057caff
warrior <1986411713@qq.com>
2025-02-12 14:36:29 +0800

    添加查看博客详情功能相关代码

--- src/main/java/com/hmdp/service/impl/BlogServiceImpl.java
+++ src/main/java/com/hmdp/service/impl/BlogServiceImpl.java
@@ -1,9 +1,13 @@
 package com.hmdp.service.impl;
 
+import com.hmdp.dto.Result;
 import com.hmdp.entity.Blog;
+import com.hmdp.entity.User;
 import com.hmdp.mapper.BlogMapper;
 import com.hmdp.service.IBlogService;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import com.hmdp.service.IUserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 /**
@@ -17,4 +21,25 @@ import org.springframework.stereotype.Service;
 @Service
 public class BlogServiceImpl extends ServiceImpl<BlogMapper, Blog> implements IBlogService {
 
+    @Autowired
+    private IUserService userService;
+
+    /**
+     * 根据id查询博客
+     * @param id
+     * @return
+     */
+    @Override
+    public Result queryBlogById(Long id) {
+        //1. 首先查询博客信息
+        Blog blog = getById(id);
+        if (blog == null)
+            return Result.fail("博客不存在！");
+        Long userId = blog.getUserId();
+        //2. 然后查询用户信息
+        User user = userService.getById(userId);
+        blog.setName(user.getNickName());
+        blog.setIcon(user.getIcon());
+        return Result.ok(blog);
+    }
 }
