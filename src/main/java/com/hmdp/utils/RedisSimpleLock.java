833bfc23c965dbfc0c0e62f4e2ede78ffbbefc5d
warrior <1986411713@qq.com>
2025-02-16 17:08:21 +0800

    添加两个重要知识点

--- src/main/java/com/hmdp/utils/RedisSimpleLock.java
+++ src/main/java/com/hmdp/utils/RedisSimpleLock.java
@@ -37,6 +37,13 @@ public class RedisSimpleLock implements ILock{
         String key = lock_prefix + name;
         long id = Thread.currentThread().getId();
         //使用UUID加线程ID来标识锁
+        //id_prefix能够唯一确定集群中的一台机器，id能够唯一标识机器中的一个线程，
+        //它们组合起来就能唯一确定一个集群中的一个线程
+
+
+        //在同一台机器上运行多个线程时，它们 默认是共享同一个 Spring IoC 容器 的。
+        //这是因为 Spring 容器是基于 JVM（Java Virtual Machine）实例的，而同一个
+        // JVM 运行的所有线程都会共享一个 Spring 应用上下文（IoC 容器）。
         String val = id_prefix + id;
         Boolean aBoolean = stringRedisTemplate.opsForValue().setIfAbsent(key, val, timeoutSec, TimeUnit.SECONDS);
         return BooleanUtil.isTrue(aBoolean);
